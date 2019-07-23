672f71b826409394df4e0e6dfc60fd75cb1f9027
khan@vsparrow <khan@vsparrow.com>
2019-07-23 02:21:23 +0000

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+// console.log('connecting to', url)
 
 mongoose.connect(url, {useNewUrlParser: true})
 .then( res => { 
