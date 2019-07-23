01a14f8236b05a45bd460a88219c171830a40874
khan@vsparrow <khan@vsparrow.com>
2019-07-23 01:55:06 +0000

    update GET note / remove old notes reference

--- index.js
+++ index.js
@@ -60,26 +60,26 @@ const generateId = () => {
     return maxID + 1;
 };
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true
-    }
-];
+// let notes = [
+//     {
+//         id: 1,
+//         content: 'HTML is easy',
+//         date: '2019-05-30T17:30:31.098Z',
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: 'Browser can execute only Javascript',
+//         date: '2019-05-30T18:39:34.091Z',
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: 'GET and POST are the most important methods of HTTP protocol',
+//         date: '2019-05-30T19:20:14.298Z',
+//         important: true
+//     }
+// ];
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
@@ -105,9 +105,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
-    res.status(204).end();
+    // const id = Number(req.params.id);
+    // notes = notes.filter(note => note.id !== id);
+    // res.status(204).end();
+	Note.findById(req.params.id)
+		.then(note => res.json(note.toJSON()))
 });
 
 app.post('/api/notes', (req, res) => {
